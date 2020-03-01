f496a151bd857d652e2af7ca763ed67011ea1d36
Hamid <hamid.hessamei@gmail.com>
2020-03-01 22:57:18 +0330

    Updates

---
+++ precache-manifest.8ef3ce8704f095d5b0f0ccb7561d6136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1426e5e2545b193d4bc3c102ab22e4",
+    "url": "/maktabkhone-Todo/index.html"
+  },
+  {
+    "revision": "2b73e8af28bc48a840f3",
+    "url": "/maktabkhone-Todo/static/css/main.5338f779.chunk.css"
+  },
+  {
+    "revision": "13abf8c863527dc661dc",
+    "url": "/maktabkhone-Todo/static/js/2.2a3baa0e.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/maktabkhone-Todo/static/js/2.2a3baa0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b73e8af28bc48a840f3",
+    "url": "/maktabkhone-Todo/static/js/main.c6521aaf.chunk.js"
+  },
+  {
+    "revision": "53cda1f08ec2079b02ab",
+    "url": "/maktabkhone-Todo/static/js/runtime-main.867e085a.js"
+  }
+]);
